b7ebaf7587cc891388ea0084c4babcc962295a3d
Meg Yahl <anotherCookieJar@gmail.com>
2018-03-15 20:13:10 -0500

    Visual Studio Scaffolding

---
+++ MvcBootstrap.Tests/Controllers/ComponentsControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcBootstrap;
+using MvcBootstrap.Controllers;
+
+namespace MvcBootstrap.Tests.Controllers
+{
+	[TestClass]
+	public class ComponentsControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			ComponentsController controller = new ComponentsController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void About()
+		{
+			// Arrange
+			ComponentsController controller = new ComponentsController();
+
+			// Act
+			ViewResult result = controller.Alerts() as ViewResult;
+
+			// Assert
+			Assert.AreEqual("Alerts go here.", result.ViewBag.Message);
+		}
+	}
+}
---
+++ MvcBootstrap/Controllers/ComponentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBootstrap.Controllers
+{
+	public class ComponentsController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Alerts()
+		{
+			ViewBag.Message = "Alerts go here.";
+
+			return View();
+		}
+	}
+}
